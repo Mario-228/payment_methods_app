b6b60c255998928c0450c2c603337d73dcf1aefb
MarioYoussef <marioyoussef08@gmail.com>
2025-02-17 17:12:20 +0200

    create stripe service class

--- lib/core/util/stripe_service/stripe_service.dart
+++ lib/core/util/stripe_service/stripe_service.dart
@@ -1,1 +1,15 @@
-class StripeService {}
+import 'package:payment_methods/core/util/api_services/api_keys.dart';
+import 'package:payment_methods/core/util/api_services/api_services.dart';
+import 'package:payment_methods/core/util/models/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:payment_methods/core/util/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel model) async {
+    var response = await ApiServices().postData(
+        parameters: model.toJson(),
+        url: ApiKeys.createIntentUrl,
+        token: ApiKeys.secretkey);
+    return PaymentIntentModel.fromJson(response.data);
+  }
+}
