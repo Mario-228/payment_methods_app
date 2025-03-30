6171b9423e063afa508ba68276b4795fe8768846
MarioYoussef <marioyoussef08@gmail.com>
2025-03-30 23:12:29 +0200

    complete stripe to save card information

--- lib/core/util/models/payment_intent_input_model/payment_intent_input_model.dart
+++ lib/core/util/models/payment_intent_input_model/payment_intent_input_model.dart
@@ -5,7 +5,8 @@ class PaymentIntentInputModel {
   PaymentIntentInputModel({
     required this.amount,
     required this.currency,
-    required this.customerId,
+    required this.customerId, //can be optional parameters , because the customer may be considered as a guest
+    //guest means that the user wants to make a payment without creating any services
   });
 
   Map<String, dynamic> toJson() =>
